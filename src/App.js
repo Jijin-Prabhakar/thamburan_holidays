aa4f2c8d99b69c4a2907c233a62d9512afdd9264
Jijin-Prabhakar <jijinprabhakar007@gmail.com>
2021-09-02 14:11:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import BannerContainer from "./components/BannerContainer/BannerContainer";
 import GalleryContainer from "./components/GalleryContainer/GalleryContainer";
+import ReviewContainer from "./components/ReviewContainer/ReviewContainer";
+
+import { useState } from "react";
+import reviewData from "./review.json";
 
 function App() {
+  const [review, setReview] = useState(reviewData);
+
+  const addReview = (userInput) => {
+    let copy = [...review, { id: review.length + 1, review: userInput }];
+    setReview(copy);
+  };
+
   return (
     <div className="App">
       <Header />
       <BannerContainer />
       <GalleryContainer />
+      <ReviewContainer addReview={addReview} />
     </div>
   );
 }
---
+++ src/components/ReviewContainer/ReviewContainer.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Element } from "react-scroll";
+import reviewData from "../../review.json";
+import "./ReviewContainer.css";
+
+import Rating from "@material-ui/lab/Rating";
+import Box from "@material-ui/core/Box";
+
+const ReviewContainer = ({ addReview }) => {
+  const [userInput, setUserInput] = useState();
+
+  const [review, setReview] = useState(reviewData);
+
+  const [value, setValue] = React.useState(4);
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(e);
+    addReview(userInput);
+    setUserInput("");
+  };
+
+  return (
+    <Element className="container review_Container" id="reviews">
+      <h1>Reviews</h1>
+      <div className="review_Container_mappedItems_form d-flex">
+        <div className="reviewContainer_mappedItems d-flex">
+          {review.map((item) => {
+            return (
+              <div className="reviewsContainer_review mt-4" key={item.id}>
+                <h3>{item.name}</h3>
+                <Box
+                  component="fieldset"
+                  mb={3}
+                  borderColor="transparent"
+                  className="rating_box"
+                >
+                  <Rating name="read-only" value={value} readOnly />
+                </Box>
+                <section>
+                  <p>{item.review}</p>
+                </section>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="reviewContainer_inputForm">
+        <form action="#" className="review_Container_form mt-5">
+          <input type="text" placeholder="Name.." />
+          <br />
+          <textarea
+            name="review"
+            cols="30"
+            rows="10"
+            value={userInput}
+            placeholder="Enter your valid comments here..."
+            onChange={handleChange}
+          />
+          <section className="review_Container_button_section">
+            <button onClick={handleSubmit}>Submit</button>
+          </section>
+        </form>
+      </div>
+    </Element>
+  );
+};
+
+export default ReviewContainer;
