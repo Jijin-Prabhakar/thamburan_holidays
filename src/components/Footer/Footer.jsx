3c25984c3ffa3c65a51c73627a9668efe67ea76f
Jijin-Prabhakar <jijinprabhakar007@gmail.com>
2021-09-08 16:53:34 +0530

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,121 +1,110 @@
 import React from "react";
 import { Element } from "react-scroll";
 import "./Footer.css";
+import thamburan from "../../assets/thamburan1.jpg";
 
 const Footer = () => {
   return (
     <div className="footerContainer">
-      <footer className="text-center text-lg-start bg-dark text-muted footerContainer">
-        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-          <div className="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-          </div>
-          <div>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-google"></i>
-            </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-github"></i>
-            </a>
-          </div>
-        </section>
-        <section className="">
-          <div className="container text-center text-md-start mt-5">
-            <div className="row mt-3">
-              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">
-                  <i className="fas fa-gem me-3"></i>Company name
-                </h6>
-                <p>
-                  Here you can use rows and columns to organize your footer
-                  content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                  elit.
-                </p>
+      <footer className="text-center text-lg-start bg-dark text-muted footerContainer pt-3">
+        <section className="mb-0">
+          <div className="container text-center text-md-start p-2">
+            <div className="row">
+              <div className="col-md-3 col-lg-4 col-xl-3 d-flex mx-auto thamburan_footer_head">
+                <img
+                  src={thamburan}
+                  alt="thamburan"
+                  className="thamburan_footer_logo"
+                />
               </div>
-              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-4">
+                <h6 className="text-uppercase fw-bold mb-4">SERVICES</h6>
                 <p>
                   <a href="#!" className="text-reset">
-                    Angular
+                    Tour Packages
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    React
+                    Rent Vehicles
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Vue
+                    Taxi Services
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Laravel
+                    Other Services
                   </a>
                 </p>
               </div>
-              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-4">
+                <h6 className="text-uppercase fw-bold mb-4">PACKAGES</h6>
                 <p>
                   <a href="#!" className="text-reset">
-                    Pricing
+                    Indian Tour Packages
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Settings
+                    Abroad Tour Packages
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Orders
+                    Honeymoon Packages
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Help
+                    Kerala Special Tour Packages
                   </a>
                 </p>
               </div>
-              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mt-4">
                 <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
                 <p>
-                  <i className="fas fa-home me-3"></i> New York, NY 10012, US
+                  <i className="fas fa-home me-3"></i> Palakkad, Kerala, India.
                 </p>
                 <p>
                   <i className="fas fa-envelope me-3"></i>
-                  info@example.com
+                  &nbsp;thamburanholidays@gmail.com
                 </p>
                 <p>
-                  <i className="fas fa-phone me-3"></i> + 01 234 567 88
+                  <i className="fas fa-phone me-3"></i> + 91 9847519710
                 </p>
                 <p>
-                  <i className="fas fa-print me-3"></i> + 01 234 567 89
+                  <i className="fas fa-print me-3"></i> + 91 9846069563
                 </p>
               </div>
             </div>
           </div>
         </section>
+        <section className="d-flex justify-content-center justify-content-lg-center p-3 border-bottom">
+          <div>
+            <a href="https://www.facebook.com/" className="me-4 text-reset">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="https://twitter.com/" className="me-4 text-reset">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="https://www.google.com/" className="me-4 text-reset">
+              <i className="fab fa-google"></i>
+            </a>
+            <a href="https://www.instagram.com/" className="me-4 text-reset">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
+        </section>
         <div
-          className="text-center p-4"
+          className="text-center thamburan_link p-2"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
         >
-          © 2021 Copyright:
-          <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
-            MDBootstrap.com
+          © 2021 Copyright :
+          <a className="text-reset fw-bold" href="#">
+            &nbsp;www.thamburanholidays.com
           </a>
         </div>
       </footer>
